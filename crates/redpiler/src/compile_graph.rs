cf76b5171c249f4f253c55c632f8933305323641
Harrison <35530362+Nano112@users.noreply.github.com>
2025-11-07 22:48:23 +1100

    fix: remove CustomIO node type (use original types instead)

--- crates/redpiler/src/compile_graph.rs
+++ crates/redpiler/src/compile_graph.rs
@@ -23,7 +23,6 @@ pub enum NodeType {
     Trapdoor,
     Wire,
     Constant,
-    CustomIO, // Custom IO nodes can inject and monitor redstone signals
     NoteBlock {
         instrument: Instrument,
         note: u32,
